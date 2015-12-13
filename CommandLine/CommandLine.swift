27e5b64e68ae965e9545510f9404b3e5b3c851ab
Guillaume Lessard <dhtnstff@gmail.com>
2015-12-13 06:41:52 -0700

    Import glibc on Linux

--- CommandLine/CommandLine.swift
+++ CommandLine/CommandLine.swift
@@ -16,7 +16,11 @@
  */
 
 /* Required for setlocale(3) */
-import Darwin
+#if os(OSX)
+  import Darwin
+#elseif os(Linux)
+  import Glibc
+#endif
 
 let ShortOptionPrefix = "-"
 let LongOptionPrefix = "--"
