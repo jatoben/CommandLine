4d9ef36f4030caa1fb5f8070c107cc25f46ea960
Ingmar Stein <IngmarStein@users.noreply.github.com>
2016-08-17 21:49:52 +0200

    Linux: update test module name

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -16,6 +16,6 @@
  */
 
 import XCTest
-@testable import CommandLineKitTestSuite
+@testable import CommandLineKitTests
 
 XCTMain([testCase(CommandLineTests.allTests), testCase(StringExtensionTests.allTests)])
