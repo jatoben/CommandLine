d79f70e6b9da847bc4b745d087dfb1c2f6d7b128
Andrew Grant <andrew@swprsolutions.com>
2018-03-17 17:33:47 -0400

    Swift 4 conversion compile error fix.

--- CommandLineKit/CommandLine.swift
+++ CommandLineKit/CommandLine.swift
@@ -310,12 +310,12 @@ public class CommandLine {
       }
 
       /* Remove attached argument from flag */
-      let splitFlag = flagWithArg.split(by: argumentAttacher, maxSplits: 1)
+      let splitFlag = flagWithArg.split(separator: argumentAttacher, maxSplits: 1)
       let flag = splitFlag[0]
-      let attachedArg: String? = splitFlag.count == 2 ? splitFlag[1] : nil
+      let attachedArg: String? = splitFlag.count == 2 ? String(splitFlag[1]) : nil
 
       var flagMatched = false
-      for option in _options where option.flagMatch(flag) {
+      for option in _options where option.flagMatch(String(flag)) {
         let vals = self._getFlagValues(idx, attachedArg)
         guard option.setValue(vals) else {
           throw ParseError.invalidValueForOption(option, vals)
