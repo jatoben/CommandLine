84a8b905f44f73303a537f95730ffcbc960abd06
kingcos <2821836721v@gmail.com>
2018-04-13 18:35:31 +0800

    Remove String's character uses

--- CommandLineKit/CommandLine.swift
+++ CommandLineKit/CommandLine.swift
@@ -124,7 +124,7 @@ public class CommandLine {
    */
   public var maxFlagDescriptionWidth: Int {
     if _maxFlagDescriptionWidth == 0 {
-      _maxFlagDescriptionWidth = _options.map { $0.flagDescription.characters.count }.sorted().first ?? 0
+      _maxFlagDescriptionWidth = _options.map { $0.flagDescription.count }.sorted().first ?? 0
     }
 
     return _maxFlagDescriptionWidth
@@ -301,7 +301,7 @@ public class CommandLine {
       }
 
       let skipChars = arg.hasPrefix(longOptionPrefix) ?
-        longOptionPrefix.characters.count : shortOptionPrefix.characters.count
+        longOptionPrefix.count : shortOptionPrefix.count
       let flagWithArg = arg[arg.index(arg.startIndex, offsetBy: skipChars)..<arg.endIndex]
 
       /* The argument contained nothing but ShortOptionPrefix or LongOptionPrefix */
@@ -332,9 +332,9 @@ public class CommandLine {
       }
 
       /* Flags that do not take any arguments can be concatenated */
-      let flagLength = flag.characters.count
+      let flagLength = flag.count
       if !flagMatched && !arg.hasPrefix(longOptionPrefix) {
-        let flagCharactersEnumerator = flag.characters.enumerated()
+        let flagCharactersEnumerator = flag.enumerated()
         for (i, c) in flagCharactersEnumerator {
           for option in _options where option.flagMatch(String(c)) {
             /* Values are allowed at the end of the concatenated flags, e.g.
