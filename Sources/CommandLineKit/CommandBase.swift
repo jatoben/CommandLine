0468eca88ae83bf2f6c060a493b50dc0034a917f
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-12-16 18:10:34 +0100

    Adding CommandBase

---
+++ Sources/CommandLineKit/CommandBase.swift
@@ -0,0 +1,56 @@
+//
+//  CommandBase.swift
+//  CommandlineKit
+//
+//  Created by Benoit Pereira da silva on 15/12/2018.
+//
+import Foundation
+
+protocol PrintDelegate {
+    func printIfVerbose(_ message: Any)
+    func printAlways(_ message: Any)
+}
+
+/// Base command implementing common behavior for all commands
+open class CommandBase: PrintDelegate {
+    
+    open var isVerbose=true
+    
+    private static let _cli = CommandLine()
+
+    init(){
+        CommandBase._cli.usesSubCommands = true
+    }
+    
+    open func addOptions(options: Option...) {
+        for o in options {
+            CommandBase._cli.addOption(o)
+        }
+    }
+
+    open func parse() -> Bool {
+        do {
+            try CommandBase._cli.parse()
+            return true
+        } catch {
+            CommandBase._cli.printUsage()
+            exit(EX_USAGE)
+        }
+    }
+    
+    open func printIfVerbose(_ message: Any) {
+        if self.isVerbose {
+            self.printAlways(message)
+        }
+    }
+    
+    /**
+     Versatile print method.
+     
+     - parameter string: the message
+     */
+    open func printAlways(_ message: Any) {
+        print("\(message)")
+    }
+    
+}
